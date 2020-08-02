e804545c90463bc6a26f303490e5bd690800160d
leng-green <2315662789@qq.com>
2020-08-02 12:06:46 +0800

    security初体验

---
+++ src/main/java/com/test/MySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//引导类
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
